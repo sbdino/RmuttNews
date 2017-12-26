90d4efcbc52b534ff3ae5b32e8b0441e3120889c
sbdino <reborn-314@hotmail.com>
2017-12-26 18:27:24 +0700

    Check Duplicate Email Success

--- app/src/main/java/ball/mac/no/rmuttnews/fragment/RegisterFragment.java
+++ app/src/main/java/ball/mac/no/rmuttnews/fragment/RegisterFragment.java
@@ -1,5 +1,8 @@
 package ball.mac.no.rmuttnews.fragment;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -11,7 +14,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import ball.mac.no.rmuttnews.R;
+import ball.mac.no.rmuttnews.ServiceActivity;
+import ball.mac.no.rmuttnews.utility.GetAllData;
+import ball.mac.no.rmuttnews.utility.MyAlert;
 import ball.mac.no.rmuttnews.utility.MyConstant;
 import ball.mac.no.rmuttnews.utility.PostNewUserToServer;
 
@@ -28,24 +37,26 @@ public class RegisterFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-//        Register Controll
+//        Register Control
 
-        registerControll();
+        registerControl();
 
     }//Main Method
 
-    private void registerControll() {
+    private void registerControl() {
         Button button = getView().findViewById(R.id.btnRegister);
+        final EditText email = getView().findViewById(R.id.etEmail);
+        final EditText password = getView().findViewById(R.id.etPassword);
+        final EditText firstname = getView().findViewById(R.id.etFirstname);
+        final EditText lastname = getView().findViewById(R.id.etLastname);
 
         button.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("ResourceAsColor")
             @Override
             public void onClick(View v) {
 
 //                Initial EditText
-                EditText email = getView().findViewById(R.id.etEmail);
-                EditText password = getView().findViewById(R.id.etPassword);
-                EditText firstname = getView().findViewById(R.id.etFirstname);
-                EditText lastname = getView().findViewById(R.id.etLastname);
+
 
 //                Get Value From EditText
                 emailString = email.getText().toString().trim();
@@ -60,13 +71,50 @@ public class RegisterFragment extends Fragment {
                 }
                 else{
 //                    NO Space
+                    checkEmail();
 
-                    uploadToServer();
+                    email.setTextColor(Color.RED);
+
                 }
 
             }//onClick
         });
     }
+
+    private void checkEmail() {
+        //checkEmailStart
+        try {
+
+            MyConstant myConstant = new MyConstant();
+            MyAlert myAlert = new MyAlert(getActivity());
+            GetAllData getAllData = new GetAllData(getActivity());
+            getAllData.execute(myConstant.getUrlGetUser());
+
+            String jsonString = getAllData.get();
+            Log.d("23DecV1", "JSON ==>: " + jsonString);
+
+            JSONArray jsonArray = new JSONArray(jsonString);
+            String[] columnStrings = myConstant.getColumnUserString();
+            Boolean b = true ;//Email True
+
+            for (int i = 0 ; i<jsonArray.length();i++) {
+                //                Duplicate email
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                if (emailString.equals(jsonObject.getString(columnStrings[1]))) {
+                    b = false;
+                    myAlert.normalDialog("Duplicate Email","Please try again Email");
+                }
+            }//for
+
+            if (b)
+            {
+                uploadToServer();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+//    checkEmailEnd
 
     private void uploadToServer() {
         try {
@@ -80,7 +128,7 @@ public class RegisterFragment extends Fragment {
             if (Boolean.parseBoolean(result)) {
                 getActivity().getSupportFragmentManager().popBackStack();
             } else {
-                Toast.makeText(getActivity(),"Cann not Upload New User ",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(),"Can not Upload New User ",Toast.LENGTH_SHORT).show();
             }
 
         } catch (Exception e) {
